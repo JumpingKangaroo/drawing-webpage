c1bd4cefd27878d746ce7f73f06ad77f34f59252
JumpingKangaroo <lambertsonchris@gmail.com>
2019-01-17 21:31:47 -0500

    Started div init code

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function initDivArray() {
+  var father = document.getElementById("#container");
+  var div;
+  for (var i = 0; i < 4; i++) {
+    div = document.createElement("div");
+    div.setAttribute("id", "row" + String(i));
+    for (var j = 0; j < 4; j++) {
+      
+    }
+  }
+    
+}
