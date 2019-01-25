fb143fcafc3cc0b772738d0927d43829a3bbcbf7
JumpingKangaroo <lambertsonchris@gmail.com>
2019-01-24 21:43:43 -0500

    Update

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 
-function initDivArray() {
-  var father = document.getElementById("#container");
-  var div;
-  for (var i = 0; i < 4; i++) {
-    div = document.createElement("div");
-    div.setAttribute("id", "row" + String(i));
-    for (var j = 0; j < 4; j++) {
-      
+function createDiv(father, id) {
+  let newDiv = document.createElement("div");
+  newDiv.setAttribute("id", id);
+  father.appendChild(newDiv);
+  return newDiv;
+}
+
+function findStyleSheet(title) {
+  console.log(document.styleSheets)
+  for (var i = 0; i < document.styleSheets.length; i++) {
+    var sheet = document.styleSheets[i];
+    console.log("title " + String(i) + ": " + sheet.title);
+    if (sheet.title == title)
+      return sheet;
+  }
+  console.log("Couldn't find stylesheet: " + title);
+  return null;
+}
+
+function initDivArray(numPerSide) {
+  // First, set the width and height of the grid
+  var gridSheet = findStyleSheet("style");
+  gridSheet.insertRule("")
+  var container = document.getElementById("container");
+  var boxNum = 0;
+  var newDiv;
+  for (var i = 0; i < numPerSide; i++) {
+    for (var j = 0; j < numPerSide; j++) {
+      newDiv = createDiv(container, boxNum);
+      boxNum++;
     }
   }
     
 }
+
+initDivArray()
