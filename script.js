74f3a16327de70b41e587998274179d398a05196
JumpingKangaroo <lambertsonchris@gmail.com>
2019-01-25 10:34:39 -0500

    Finished full (but no extra) functionality. Not going to do anything else witih it at the moment. Works though!

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-function resetBox() {
-  this.classList.remove('clickedBox');
-  this.classList.add('blankBox');
+// Reset a given div box to the original wheat color
+function resetBox(div) {
+  div.classList.remove('clickedBox');
+  div.classList.add('blankBox');
 }
 
+// Empties the given container of all child elements
+function emptyContainer(container) {
+  while (container.firstChild != null) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+// Called when mouseover on a box, changes it to yellow
 function changeColor() {
   this.classList.remove('blankBox');
   this.classList.add('clickedBox');
 }
 
-function initDivs() {
+// Initialize the n x n div array, and append them to the container div
+function initDivs(n) {
   var container = document.querySelector("#container");
-
-  for (var i = 0; i < 256; i++) {
+  var numOfDivs = n * n;
+  for (var i = 0; i < numOfDivs; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("blankBox");
     newDiv.addEventListener('mouseover', changeColor);
@@ -19,5 +29,22 @@ function initDivs() {
   }
 }
 
+// Get new n (height and width of drawing thing)
+function resetButtonPushed() {
+  emptyContainer(document.querySelector("#container"));
+  var n = parseInt(prompt("Enter a new width and height (in boxes) for the canvas", "16"));
+  var container = document.querySelector("#container");
+  container.style.gridTemplateColumns = "repeat(" + String(n) + ", auto)";
+  container.style.gridTemplateRows = "repeat(" + String(n) + ", auto)";
+  initDivs(n);
+}
 
-initDivs();
+// Initialize the button listener event
+function initButton() {
+  var resetButton = document.querySelector("#reset");
+  resetButton.addEventListener("click", resetButtonPushed);
+}
+
+// Initialize the button, and the initial 16 x 16 canvas
+initDivs(16);
+initButton();
